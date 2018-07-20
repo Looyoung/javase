e1ca14ca47facae9b9dfe83d7e5be61cd2223065
Louis <18813291925@163.com>
2018-07-20 16:33:31 +0800

    test sleep method

---
+++ src/MutilThread/MyThread07.java
@@ -0,0 +1,22 @@
+package MutilThread;
+
+public class MyThread07 extends Thread{
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("run thread name ="+this.getName()+" begin");
+            Thread.sleep(2000);
+            System.out.println("run thread name ="+this.getName()+" end");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread07 myThread07 = new MyThread07();
+        System.out.println("begin = "+System.currentTimeMillis());
+        myThread07.start();
+        System.out.println("end = "+System.currentTimeMillis());
+    }
+}
